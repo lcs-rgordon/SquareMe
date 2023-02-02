67b234c1a2ecd23b8ef26853a827b28b5bfbaa83
Russell Gordon <rgordon@lcs.on.ca>
2023-02-02 14:23:25 -0500

    We have written a computed property to get the input as an optional Double. Step 2 of 4

--- SquareMe/Views/CalculationView.swift
+++ SquareMe/Views/CalculationView.swift
@@ -16,6 +16,19 @@ struct CalculationView: View {
     
     // MARK: Computed properties
     
+    // Convert the input given into an optional Double
+    var inputGivenAsOptionalDouble: Double? {
+        
+        // Attempt to create the optional Double
+        guard let inputGivenAsDouble = Double(inputGiven) else {
+            // User gave us bad input ... cannot convert to double
+            return nil
+        }
+        
+        // We have an actual double, so return it
+        return inputGivenAsDouble
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
