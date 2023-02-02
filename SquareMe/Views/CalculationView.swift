b55f7beec68b262bae1f7aeb5eb948cd0b5595f0
Russell Gordon <rgordon@lcs.on.ca>
2023-02-02 14:14:27 -0500

    Added a TextField to collect user input.

--- SquareMe/Views/CalculationView.swift
+++ SquareMe/Views/CalculationView.swift
@@ -8,10 +8,20 @@
 import SwiftUI
 
 struct CalculationView: View {
+    
+    // MARK: Stored properties
+    
+    // Keeps the input given by user
+    @State var inputGiven = ""
+    
+    // MARK: Computed properties
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("Hello world")
+                TextField("5.5", text: $inputGiven)
+                    .font(.largeTitle)
+                    .padding()
             }
             .navigationTitle("Square Me")
         }
